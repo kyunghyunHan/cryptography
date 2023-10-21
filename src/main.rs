92b59dd08e6aa20435e724fdf5134c357dac335e
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-10-21 20:19:19 +0900

    feat:euclid

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ mod part2;
 mod part3;
 fn  main(){
   part1::eucild::main();
+  part1::extended_eucild::main();
   // part2::vigenere_cipher::main();
 }
--- src/part1/extended_eucild.rs
+++ src/part1/extended_eucild.rs
@@ -1,53 +1,59 @@
-fn extended_euclid(a: i32, b: i32) -> Option<(i32, i32, i32)> {
-  let mut r1 = a;
-  let mut r2 = b;
-  let mut s1 = 1;
-  let mut s2 = 0;
-  let mut t1 = 0;
-  let mut t2 = 1;
-  let mut r;
-  let mut s;
-  let mut t;
-  let mut q;
+fn extended_eucild(a:i32,p:i32)->i32{
+    let mut q:i32;
+    let mut x2:i32;
+    let mut x3:i32;
+    let mut y2:i32;
+    let mut y3:i32;
+    let mut z2:i32;
+    let mut z3:i32;
 
-  while r2 != 0 {
-      q = r1 / r2;
-      // gcd 계산
-      r = r1 % r2;
-      r1 = r2;
-      r2 = r;
+    //0일떄 처리
+    if p==0{
+        return 0;
+    }
+    //초기값 설정
+    y3= p;
+    x3= a;
+    x2=0;
+    y2= 1;
+   //역수를 구하기 위해 y3가 1이 될떄까지 몫과 나머지를 반복적으로 구한다.
+    while y3 >1 {
+        q= x3/y3;
 
-      // s 계산
-      s = s1 - q * s2;
-      s1 = s2;
-      s2 = s;
 
-      // t 계산
-      t = t1 - q * t2;
-      t1 = t2;
-      t2 = t;
-  }
+        z3= x3-(q*y3);
+        x3= y3;
+        y3= z3;
 
-  r = r1;
-  s = s1;
-  t = t1;
-  
-  println!("gcd({}, {}) = {}, s = {}, t = {}", a, b, r, s, t);
-  println!("{} x {} + {} x {} = {}", a, s, b, t, r);
+        z2= x2-(q*y2);
+        x2= y2;
+        y2= z2;
 
-  if r == 1 {
-      if t < 0 {
-          t += a;
-      }
-      Some((a, s, t))
-  } else {
-      None
-  }
+     
+    }
+    //역원이 존재하면 역원을 반환하는데 음수이면 양의 정수로 변환
+    if y3==1 {
+            
+        //음수일 경우 모듈러 a를 더해준다.
+        if y2 <0 {
+            y2+=a;
+            return y2;
+
+            
+        }
+        return 0;
+    }
+
+
+    0
 }
-
 pub fn main() {
-  let inverse = extended_euclid(26, 11);
-  if let Some((a, s, t)) = inverse {
-      println!("역원: {}", t);
+
+  let mut inverse= extended_eucild(100,23);
+  if inverse !=0{
+     println!("두수의 역원 {}",inverse)
+  }else{
+    println!("{}","역원이 존재하지 않습니다.")
+
   }
 }
